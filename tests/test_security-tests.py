d283242c18fc6c01c9ec1f08e907bf68f4712c98
= <=>
2025-12-12 10:28:12 +0000

    Fix fault in pytest file

--- tests/test_security-tests.py
+++ tests/test_security-tests.py
@@ -42,4 +42,4 @@ def test_auth_levels(client):
         print(session)
         device_response=client.get("/assets/17", follow_redirects=True)
         user_response=client.get("/users", follow_redirects=True)
-        assert device_response.request.path == '/assets' and user_response.request.path != "/home"
+        assert device_response.request.path == '/assets' and user_response.request.path == "/home"
